29a1630b9f0647f20cd4c3742ab1890a7fc36e05
matteopoliti <matteo.politi17@gmail.com>
2024-04-09 12:20:13 +0200

    Feat: added pagination function

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -3,6 +3,27 @@
         <div class="row"> 
                 <ProjectCard v-for="(element, index) in arrayProjects" :key="element.id" :propsElement="element" />
         </div>
+
+        <div class="pagination">
+           
+            <ul>
+                <li>
+                    <button @click="getProjects( currentPage - 1 )" :class="{'disabled': currentPage === 1}">
+                        <i class="fa-solid fa-arrow-left"></i> Prev
+                    </button>
+                </li>
+
+                <li v-for="(item, index) in lastPage" :key="index">
+                    <button @click="getProjects( item )" :class="{'active': currentPage === item}">{{ item }}</button>
+                </li>
+
+                <li>
+                    <button @click="getProjects( currentPage + 1 )" :class="{'disabled': currentPage === lastPage}">
+                        Next <i class="fa-solid fa-arrow-right"></i>
+                    </button>
+                </li>
+            </ul>
+        </div>
     </main>
 </template>
 
@@ -17,21 +38,30 @@ export default {
     data() {
 
         return {
-            arrayProjects: []
+            arrayProjects: [],
+            currentPage: '',
+            lastPage: ''
         }
     },
     methods: {
-        getProjects() {
-            axios.get('http://127.0.0.1:8000/api/projects')
+        getProjects( projectApiPage ) {
+            axios.get('http://127.0.0.1:8000/api/projects',
+                { 
+                    params: {
+                        page: projectApiPage
+                    }
+                })
                 .then(res => {
                     // console.log(res.data.projects)
 
-                    this.arrayProjects = res.data.projects
+                    this.arrayProjects = res.data.projects.data
+                    this.currentPage = res.data.projects.current_page
+                    this.lastPage = res.data.projects.last_page
                 })
-        }
+        },
     },
     mounted() {
-        this.getProjects()
+        this.getProjects( 1 )
     },
 }
 </script>
@@ -39,8 +69,47 @@ export default {
 <style lang="scss" scoped>
 .row {
     display: flex;
+    justify-content: center;
     align-items: end;
-    gap: 20px;
+    gap: 40px;
     flex-wrap: wrap;
+}
+.pagination{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-block: 50px;
+   
+    ul{
+        list-style-type: none;
+        display: flex;
+        background: rgba(255, 255, 255, 0.05);
+        .disabled{
+            pointer-events: none;
+            background-color: black;
+            color: #ccc;
+        }
+        .active{
+            background: rgba(0, 0, 0, 0.5);
+        }
+        li{
+            button{
+                border: none;
+                padding: 11px;
+                padding: 20px 25px;
+                color: #f9a300;
+                font-weight: 500;
+                font-size: 16px;
+                cursor: pointer;
+                &:hover{
+                    background: rgba(0, 0, 0, 0.5);
+                }
+                i{
+                    margin-inline: 5px;
+                }
+            }
+        }
+    }
+    
 }
 </style>
